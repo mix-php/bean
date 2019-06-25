35c512579c6aaca17243e07490ee8189c86729d4
Jian Liu <onanying@126.com>
2019-06-25 12:09:16 +0800

    使用php-di/phpdoc-reader来处理var注解

--- src/BeanInjector.php
+++ src/BeanInjector.php
@@ -49,66 +49,41 @@ class BeanInjector
      * @param $object
      * @param array $properties
      * @return mixed
+     * @throws \PhpDocReader\AnnotationException
      * @throws \ReflectionException
      */
     public static function inject($object, array $properties)
     {
         foreach ($properties as $name => $value) {
-            // 导入
-            $object->$name = $value;
             // 注释类型检测
             $class      = get_class($object);
             $reflection = new \ReflectionClass($class);
             if (!$reflection->hasProperty($name)) {
                 continue;
             }
-            $docComment = $reflection->getProperty($name)->getDocComment();
-            $var        = self::var($docComment);
-            if (!$var) {
+            $property      = $reflection->getProperty($name);
+            $reader        = new PhpDocReader();
+            $propertyClass = $reader->getPropertyClass($property);
+            if (!$propertyClass) {
                 continue;
             }
-            if (substr($var, -2) === '[]') {
+            if (substr($propertyClass, -2) === '[]') {
                 // 当前的doc标注里面这是一个数组，去掉数组的尾巴
-                $var = substr($var, 0, -2);
+                $propertyClass = substr($propertyClass, 0, -2);
                 // 这时候当前的$value已经是个被依赖注入自动维护的实例数组了 不需要特殊处理
             } else {
                 // 不是数组，弄成临时数组 方便下面遍历检查
-                $value = [$value];
+                $values = [$value];
             }
-            if (!interface_exists($var) && !class_exists($var)) {
-                throw new InjectException("Interface or class not found, class: {$class}, property: {$name}, @var: {$var}");
-            }
-            foreach ($value as $v) {
-                if (!($v instanceof $var)) {
-                    throw new InjectException("The type of the imported property does not match, class: {$class}, property: {$name}, @var: {$var}");
+            foreach ($values as $v) {
+                if (!($v instanceof $propertyClass)) {
+                    throw new InjectException("The type of the imported property does not match, class: {$class}, property: {$name}, @var: {$propertyClass}");
                 }
             }
+            // 导入
+            $object->$name = $value;
         }
         return $object;
-    }
-
-    /**
-     * 获取注释中var的值
-     * @param $docComment
-     * @return string
-     */
-    protected static function var($docComment)
-    {
-        $var = '';
-        if (!$docComment) {
-            return $var;
-        }
-        $key   = '@var';
-        $len   = 4;
-        $start = strpos($docComment, $key);
-        $end   = strpos($docComment, '*', $start + $len);
-        if ($start !== false && $end !== false) {
-            $tmp = substr($docComment, $start + $len, $end - $start - $len);
-            $tmp = explode(' ', trim($tmp));
-            $var = array_shift($tmp);
-            $var = substr($var, 0, 1) === '\\' ? substr($var, 1) : '';
-        }
-        return $var;
     }
 
 }
